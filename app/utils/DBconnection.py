0e84a442b990ee923773e78b72767707dc0dde22
Mafika <rudolphmafika@gmail.com>
2025-06-03 17:59:25 +0200

    PostgreSQL migration - backend logic2

--- app/utils/DBconnection.py
+++ app/utils/DBconnection.py
@@ -1,7 +1,7 @@
 """
 Establish DB connection
 """
-from config import Config
+from ...config import Config
 from app import app
 app.config.from_object(Config)
 import psycopg2
--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-import psycopg2
 import os
 
-conn = psycopg2.connect(
-    host=os.getenv("DB_HOST"),
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD")
-)
+class Config:
+    POSTGRES_HOST = os.getenv("DB_HOST")
+    POSTGRES_DB = os.getenv("DB_NAME")
+    POSTGRES_USER = os.getenv("DB_USER")
+    POSTGRES_PASSWORD = os.getenv("DB_PASSWORD")
