485bfdca1362b935cb0db5a8a0552a7891896ff5
Mafika <rudolphmafika@gmail.com>
2025-06-03 17:26:31 +0200

    postgreSQLv2

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+import psycopg2
 import os
 
-class Config:
-    
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = os.environ.get('MYSQL_USER')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-    MYSQL_DB = 'SurveysDB'
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
--- createDB.sql
+++ createDB.sql
@@ -1,25 +1,25 @@
-#create database
-CREATE DATABASE SurveysDB;
+-- Create database
+CREATE DATABASE surveysdb;
 
-#select database
-USE SurveysDB;
+-- Connect to the database (use `\c surveysdb` if using psql CLI)
+-- or switch databases via your tool (e.g., pgAdmin)
 
-#create survey table
-CREATE TABLE Survey (
-	survey_id INT(4) ZEROFILL AUTO_INCREMENT PRIMARY KEY,
-	fullname VARCHAR(100) NOT NULL,
-	email VARCHAR(100) NOT NULL,
-	age INT NOT NULL,
-	contact_number VARCHAR(20) NOT NULL,
-	pizza INT NOT NULL,
-	pasta INT NOT NULL,
-	pap_and_wors INT NOT NULL,
-	other INT NOT NULL,
-	movies INT NOT NULL,
-	radio INT NOT NULL,
-	eat_out INT NOT NULL,
-	tv INT NOT NULL
+-- Create survey table
+CREATE TABLE survey (
+    survey_id SERIAL PRIMARY KEY,
+    fullname VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    contact_number VARCHAR(20) NOT NULL,
+    pizza INT NOT NULL,
+    pasta INT NOT NULL,
+    pap_and_wors INT NOT NULL,
+    other INT NOT NULL,
+    movies INT NOT NULL,
+    radio INT NOT NULL,
+    eat_out INT NOT NULL,
+    tv INT NOT NULL
 );
 
-#display table to confirm creation
-SHOW TABLES;
+-- Show tables (in psql CLI)
+-- \dt
